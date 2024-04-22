687f97aa358f43882e0c680627e95af85fa2b878
Niculina <virtual_n_r@yahoo.com>
2024-04-22 15:47:25 +0300

    router added

---
+++ src/pages/contact-us.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export function ContactUs() {
+    return (
+        <div>This is the Contact-Us page</div>
+    )
+}
---
+++ src/pages/not-found.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function NotFound() {
+    return (
+        <div>
+            <h1>Eroare 404, pagina nu exista</h1>
+        </div>
+    )
+}
